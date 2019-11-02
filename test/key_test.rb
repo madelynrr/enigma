f734efcd96b4ae469aec2a4f24c8691bd7392145
Madelyn Romero <m.romero013@gmail.com>
2019-11-02 12:47:53 -0600

    Test with stubs for number splits into four elements

--- test/key_test.rb
+++ test/key_test.rb
@@ -14,16 +14,16 @@ class KeyTest < Minitest::Test
   def test_it_has_attributes
     assert_equal String, @key.number.class
     assert_equal 5, @key.number.chars.length
-    assert_equal Integer, @key.number.chars.class
   end
 
   def test_it_splits_number_into_four_elements
-    skip
-    @key.expects(:number).returns("01234")
+    @key.expects(:number).at_least_once.returns("01234")
 
     expected = ["01", "12", "23", "34"]
 
     assert_equal expected, @key.number_to_array
   end
+
+  
 
 end
