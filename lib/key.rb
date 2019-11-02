7d84478811cd98bd32dc3d0fbf8815be5a29f0a4
Madelyn Romero <m.romero013@gmail.com>
2019-11-02 11:51:38 -0600

    Add method to turn number to string and pad with zeroes

--- lib/key.rb
+++ lib/key.rb
@@ -3,7 +3,11 @@ class Key
   attr_reader :number
 
   def initialize
-    @number = rand(99999).to_s
-    require "pry"; binding.pry
+    @number = rand(99999)
+  end
+
+  def number_to_key_characteristics
+    number_to_string = @number.to_s
+    number_to_string.rjust(5, '0')
   end
 end
