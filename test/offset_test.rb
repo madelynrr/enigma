9ec45bc73f63e686b87dcdc65c17063bdad8b3ae
Madelyn Romero <m.romero013@gmail.com>
2019-11-02 17:04:28 -0600

    Create offset test class and test it exists

---
+++ test/offset_test.rb
@@ -0,0 +1,11 @@
+require './test/test_helper'
+require './lib/offset'
+
+class OffsetTest < Minitest::Test
+
+  def test_it_exists
+    offset = Offset.new
+
+    assert_instance_of Offset, offset
+  end
+end
