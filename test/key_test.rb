69f70894af9b7c92ef631869f215d30dfa95ee8f
Madelyn Romero <m.romero013@gmail.com>
2019-11-02 11:51:13 -0600

    Add test to turn number to string and pad with zeroes

--- test/key_test.rb
+++ test/key_test.rb
@@ -12,12 +12,20 @@ class KeyTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal String, @key.number.class
-    assert_equal 5, @key.number.chars.length
+    assert_equal Integer, @key.number.class
   end
 
-  def test_it_splits_number_into_four
+  def test_number_is_five_characters_and_string
+    assert_equal String, @key.number_to_key_characteristics.class
+    assert_equal 5, @key.number_to_key_characteristics.chars.length
+  end
+
+  def test_it_splits_number_into_four_elements
     skip
+    @key.expects(:number).returns("01234")
+    expected = ["01", "12", "23", "34"]
+
+    assert_equal expected, @key.number_to_four_element_string
   end
 
 end
