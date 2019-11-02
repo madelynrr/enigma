4fd8c0894062b884fdbafb00321d9a658942293e
Madelyn Romero <m.romero013@gmail.com>
2019-11-02 12:11:02 -0600

    Refactor initialize so creates key instead of integer

--- lib/key.rb
+++ lib/key.rb
@@ -3,15 +3,15 @@ class Key
   attr_reader :number
 
   def initialize
-    @number = rand(99999)
+    @number = rand(99999).to_s.rjust(5, '0')
   end
 
-  def number_to_key
-    number_to_string = @number.to_s
-    number_to_string.rjust(5, '0')
-  end
+  # def number_to_key
+  #   number_to_string = @number.to_s
+  #   number_to_string.rjust(5, '0')
+  # end
 
-  def number_to_array
-    @number
-  end
+  # def number_to_array
+  #
+  # end
 end
