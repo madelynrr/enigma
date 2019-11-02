e03710d4911dd9eb40a39df3168ea20d5b638b9f
Madelyn Romero <m.romero013@gmail.com>
2019-11-02 10:28:23 -0600

    Add tests for exists and has attributes

---
+++ test/key_test.rb
@@ -0,0 +1,19 @@
+require './test/test_helper'
+require './lib/key'
+
+class KeyTest < Minitest::Test
+
+  def setup
+    @key = Key.new
+  end
+
+  def test_it_exists
+    assert_instance_of Key, @key
+  end
+
+  def test_it_has_attributes
+    assert_equal Integer, @key.class
+    assert_equal 5, @key.characters
+  end
+
+end
