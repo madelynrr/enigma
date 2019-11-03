59ab961cbbf40dde62c58a37391319c538d19964
Madelyn Romero <m.romero013@gmail.com>
2019-11-03 13:04:04 -0700

    Rename method so simpler to use in enigma

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -12,11 +12,8 @@ class OffsetTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_instance_of Date, @offset.date
-  end
-
-  def test_it_is_string_in_correct_format
-    assert_equal Date.today.strftime("%d%m%y"), @offset.formatted_date_to_string
+    assert_instance_of Date, @offset.today
+    assert_equal Date.today.strftime("%d%m%y"), @offset.date
   end
 
   def test_it_can_square_date
