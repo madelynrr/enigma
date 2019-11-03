20ebb856899298b15f8c9a153ce1942129214cc0
Madelyn Romero <m.romero013@gmail.com>
2019-11-03 11:53:51 -0700

    Add working code for turning offset final four into correctly formatted hash

--- lib/offset.rb
+++ lib/offset.rb
@@ -10,4 +10,26 @@ class Offset
     date.strftime("%d%m%y")
   end
 
+  def date_squared
+    formatted_date_to_string.to_i * formatted_date_to_string.to_i
+  end
+
+  def offsets_as_strings
+    all_ints = date_squared.to_s.chars
+
+    all_ints[-4..-1]
+  end
+
+  def offsets_in_hash
+    letters_array = ["A", "B", "C", "D"]
+
+    letters_and_offsets = letters_array.zip(offsets_as_strings)
+
+    hash = {}
+    letters_and_offsets.each do |pair|
+      hash[pair.first] = pair.last
+    end
+    hash
+  end
+
 end
