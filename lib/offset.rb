26dbb9cca97fe522e88cdab2fdfcb80d3659b80e
Madelyn Romero <m.romero013@gmail.com>
2019-11-03 12:02:42 -0700

    Refactor class to include key offset module for turning offsets into a hash

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,4 +1,7 @@
+require './lib/key_offset_module'
+
 class Offset
+  include KeyOffset
 
   attr_reader :date
 
@@ -19,17 +22,4 @@ class Offset
 
     all_ints[-4..-1]
   end
-
-  def offsets_in_hash
-    letters_array = ["A", "B", "C", "D"]
-
-    letters_and_offsets = letters_array.zip(offsets_as_strings)
-
-    hash = {}
-    letters_and_offsets.each do |pair|
-      hash[pair.first] = pair.last
-    end
-    hash
-  end
-
 end
