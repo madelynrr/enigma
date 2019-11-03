056b41089dd50d9017a3abf283a92f750a6c25fe
Madelyn Romero <m.romero013@gmail.com>
2019-11-03 15:30:29 -0700

    Refactor initialize so makes key with given value, or generates new one

--- lib/key.rb
+++ lib/key.rb
@@ -1,8 +1,19 @@
+require './lib/key_offset_module'
+
 class Key
-  
+  include KeyOffset
+
   attr_reader :number
 
-  def initialize
-    @number = rand(99999).to_s.rjust(5, '0')
+  def initialize(number = rand(99999).to_s.rjust(5, '0'))
+    @number = number
+  end
+
+  def number_to_array
+    integers = number.chars
+    individual_keys = []
+    integers.each_cons(2) {|integer| individual_keys << integer.join}
+
+    individual_keys
   end
 end
