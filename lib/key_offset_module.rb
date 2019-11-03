9a09f83c8079ca37afd991d9d79c1c4b98b2004d
Madelyn Romero <m.romero013@gmail.com>
2019-11-03 11:56:09 -0700

    Delete hashed out copies of method from key and offset classes used for writing module method

--- lib/key_offset_module.rb
+++ lib/key_offset_module.rb
@@ -13,27 +13,3 @@ module KeyOffset
     hash
   end
 end
-
-# # OFFSET
-# def offsets_in_hash
-#   letters_array = ["A", "B", "C", "D"]
-#
-#   letters_and_offsets = letters_array.zip(offsets_as_strings)
-#
-#   hash = {}
-#   letters_and_offsets.each do |pair|
-#     hash[pair.first] = pair.last
-#   end
-#   hash
-# end
-#
-# # KEY
-# def hash_of_keys
-#   letters_array = ["A", "B", "C", "D"]
-#   arrays = letters_array.zip(number_to_array)
-#   hash = {}
-#   arrays.each do |array|
-#     hash[array.first] = array.last
-#   end
-#   hash
-# end
