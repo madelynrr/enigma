cd07eb3f83335536e7520f9420a7acb16cde0eea
Madelyn Romero <m.romero013@gmail.com>
2019-11-04 15:30:03 -0700

    Delete hashed out code for final shifts now in Encrypt class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,48 +1,7 @@
-require './lib/key_offset_module'
-
 class Enigma
-  include KeyOffset
 
   def encrypt(message, key = Key.new.number, date = Offset.new.date)
     final_shifts(key, date)
   end
 
-  def test_key_is_valid(key)
-    if key.class == String && key.chars.length == 5 && key.count("0123456789") == 5
-      Key.new(key)
-    else
-      Key.new
-    end
-  end
-
-  def test_date_is_valid(date)
-    if date.class == String && date.chars.length == 6 && date.count("0123456789") == 6
-      Offset.new(date)
-    else
-      Offset.new
-    end
-  end
-
-  def key_to_hash(key)
-    key = test_key_is_valid(key)
-    key_array = key.number_to_array
-    turn_to_formatted_hash(key_array)
-  end
-
-  def date_to_hash(date)
-    date_string = test_date_is_valid(date)
-    date_squared = date_string.date_squared(date_string.date)
-    all_ints = date_squared.to_s.chars
-    final_four = all_ints[-4..-1]
-    turn_to_formatted_hash(final_four)
-  end
-
-  def final_shifts(key, date)
-    keys = key_to_hash(key)
-    offsets = date_to_hash(date)
-
-    keys.merge(offsets) do |letter, old_val, new_val|
-      old_val.to_i + new_val.to_i
-    end
-  end
 end
