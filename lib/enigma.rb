8ac07f9d691b794e74fd5bddcd9d85e2dfe5e37c
Madelyn Romero <m.romero013@gmail.com>
2019-11-03 18:04:01 -0700

    Add method to format key into hash so ready to make into final shifts

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,7 @@
+require './lib/key_offset_module'
+
 class Enigma
+  include KeyOffset
 
   def encrypt(message, key = Key.new.number, date = Offset.new.format_date)
   end
@@ -13,18 +16,23 @@ class Enigma
 
   def test_date_is_valid(date)
     if date.class == String && date.chars.length == 6 && date.count("0123456789") == 6
-      Offset.new(date)
+      Offset.new(date).date
     else
-      Offset.new
+      Offset.new.date
     end
+  end
+
+  def key_to_hash(key)
+    key = test_key_is_valid(key)
+    key_array = key.number_to_array
+    turn_to_formatted_hash(key_array)
+  end
+
+  def final_shifts(key, date)
   end
 
   # def final_shifts(key, date)
   #   key.merge(date) { |letter, key_val, date_val| key_val.to_1 += date_val.to_i}
   # end
 
-  # def format_key(key)
-  #   final_key = Key.new(key)
-  #   final_key
-  # end
 end
