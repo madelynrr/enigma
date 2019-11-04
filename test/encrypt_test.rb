0479c34c7417abd18dd4410398b597b330ba7018
Madelyn Romero <m.romero013@gmail.com>
2019-11-04 12:12:50 -0700

    Refactor Enigma class so Encrypt checks date is valid

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/key'
+require './lib/offset'
 require './lib/encrypt'
 require './lib/enigma'
 
@@ -34,6 +35,23 @@ class EncryptTest < Minitest::Test
     encrypt_4 = Encrypt.new("hello world", 10234, "040895")
     key = 01234
     assert_equal false, 01234 == encrypt_4.test_key_is_valid(key).number
+  end
+
+  def test_it_tests_date_is_valid
+    date = "040895"
+    assert_equal true, "040895" == @encrypt.test_date_is_valid(date).date
+
+    encrypt_1 = Encrypt.new("hello world", "02715", "40895")
+    date = "40895"
+    assert_equal false, "40895" == encrypt_1.test_date_is_valid(date).date
+
+    encrypt_2 = Encrypt.new("hello world", "02715", "cccccc")
+    date = "cccccc"
+    assert_equal false, "cccccc" == encrypt_2.test_date_is_valid(date).date
+
+    encrypt_3 = Encrypt.new("hello world", "02715", 110895)
+    date = 110895
+    assert_equal false, 110895 == encrypt_3.test_date_is_valid(date).date
   end
 
   def test_it_can_format_key_into_hash
