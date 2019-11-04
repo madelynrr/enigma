015df2be023bb73acd4bd0df3d7eb841bfcab3ab
Madelyn Romero <m.romero013@gmail.com>
2019-11-04 12:01:07 -0700

    Refactor enigma class to include key validity check to encrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,4 +7,12 @@ class Encrypt
     @key = key
     @date = date
   end
+
+  def test_key_is_valid(key)
+    if key.class == String && key.chars.length == 5 && key.count("0123456789") == 5
+      Key.new(key)
+    else
+      Key.new
+    end
+  end
 end
