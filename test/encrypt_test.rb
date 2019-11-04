3d8412863a6487a5f519285c1c5a316efe3bdb75
Madelyn Romero <m.romero013@gmail.com>
2019-11-04 11:59:51 -0700

    Refactor enigma class to add validity check on key to encrypt class

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,6 +1,7 @@
 require './test/test_helper'
-require './lib/enigma'
+require './lib/key'
 require './lib/encrypt'
+require './lib/enigma'
 
 class EncryptTest < Minitest::Test
 
@@ -21,5 +22,39 @@ class EncryptTest < Minitest::Test
     assert_equal "040895", @encrypt.date
   end
 
-  def test_it
+  def test_test
+    assert_equal true, @encrypt.test
+  end
+
+  def test_it_tests_key_is_valid_or_generates_new_key
+    key = "02715"
+    assert_equal "02715", @encrypt.test_key_is_valid(key).number
+
+    encrypt_2 = Encrypt.new("hello world", "2715", "040895")
+    key = "2715"
+    assert_equal false, "2715" == encrypt_2.test_key_is_valid(key).number
+
+    encrypt_3 = Encrypt.new("hello world", "class", "040895")
+    key = "class"
+    assert_equal false, "class" == encrypt_3.test_key_is_valid(key).number
+
+    encrypt_4 = Encrypt.new("hello world", 10234, "040895")
+    key = 01234
+    assert_equal false, 01234 == encrypt_4.test_key_is_valid(key).number
+  end
+
+  def test_it_can_format_key_into_hash
+    skip
+    @enigma.encrypt("hello world", "02715", "040895")
+    key = "02715"
+    expected = {
+                "A" => "02",
+                "B" => "27",
+                "C" => "71",
+                "D" => "15"
+                }
+
+    assert_equal expected, @encrypt.key_to_hash(key)
+  end
+
 end
