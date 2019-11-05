165c660e562805a213d5e10864337b801546815b
Madelyn Romero <m.romero013@gmail.com>
2019-11-05 12:50:13 -0700

    Add tests from encrypt class that overlap with functionality of encrypt class

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -47,6 +47,28 @@ class DecryptTest < Minitest::Test
     assert_equal "Invalid Date", @decrypt.test_date_is_valid(date)
   end
 
+  def test_it_can_format_key_into_array
+    key = @decrypt.key
+    expected = ["02", "27", "71", "15"]
+
+    assert_equal expected, @decrypt.key_to_array(key)
+  end
+
+  def test_it_can_format_date_into_array
+    date = "040895"
+    expected = ["1", "0", "2", "5"]
+
+    assert_equal expected, @decrypt.date_to_array(date)
+  end
+
+  def test_it_creates_final_shifts
+    key = "02715"
+    date = "040895"
+    expected = [3, 27, 73, 20]
+
+    assert_equal expected, @decrypt.final_shifts(key, date)
+  end
+
   def test_it_can_decrypt_given_the_key_and_date
     skip
     expected = {
@@ -55,7 +77,7 @@ class DecryptTest < Minitest::Test
       date: "040895"
       }
 
-    assert_equal expected, @encrypt.decrypt_message("keder ohulw!!", "02715", "040895")
+    assert_equal expected, @decrypt.decrypt_message("keder ohulw!!", "02715", "040895")
   end
 
 end
