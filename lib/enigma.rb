c4cfbcab5e0bdacac4f8bd15ade085bfa7266235
Madelyn Romero <m.romero013@gmail.com>
2019-11-05 16:27:09 -0700

    Rename Encrypt and Decrypt to Encryption and Decryption to differentiate from runner file names

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,17 @@
 require './lib/key'
 require './lib/offset'
-require './lib/encrypt'
-require './lib/decrypt'
+require './lib/encryption'
+require './lib/decryption'
 
 class Enigma
 
   def encrypt(message, key = Key.new.number, date = Offset.new.date)
-    encoded = Encrypt.new(message, key, date)
+    encoded = Encryption.new(message, key, date)
     encoded.encrypt_message
   end
 
   def decrypt(ciphertext, key, date = Offset.new.date)
-    decoded = Decrypt.new(ciphertext, key, date)
+    decoded = Decryption.new(ciphertext, key, date)
     decoded.decrypt_message
   end
 
