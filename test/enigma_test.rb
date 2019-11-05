92c8904b9ed7ef212c92c149a6381a567e718522
Madelyn Romero <m.romero013@gmail.com>
2019-11-05 13:37:56 -0700

    Include decrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,6 +4,7 @@ require './test/key_test'
 require './lib/offset'
 require './test/offset_test.rb'
 require './lib/encrypt'
+require './lib/decrypt'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
@@ -28,19 +29,31 @@ class EnigmaTest < Minitest::Test
 
   def test_it_can_encrypt_without_given_date
     enigma_1 = Enigma.new
+
     expected = {
       encryption: "jfrmqabptmj!!",
       key: "00000",
       date: "041119"
       }
 
-    assert_equal expected, enigma_1.encrypt("hello world!!", "00000")
+    assert_equal Date.today.strftime("%d%m%y"), enigma_1.encrypt("hello world!!", "00000")[:date]
   end
 
   def test_it_can_encrypt_without_given_key_or_date
     enigma_1 = Enigma.new
 
     assert_equal 13, enigma_1.encrypt("hello world!!")[:encryption].length
+  end
+
+  def test_it_can_decrypt
+    enigma_2 = Enigma.new
+    expected = {
+      decryption: "hello world!!",
+      key: "02715",
+      date: "040895"
+      }
+
+    assert_equal expected, enigma_2.decrypt("keder ohulw!!", "02715", "040895")
   end
 
 
